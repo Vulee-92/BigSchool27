fe661d7c7eeab274f1826b21f168f0fd34773466
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2021-07-03 11:15:08 +0700

    Add project files.

---
+++ BigSchool27/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool27.MvcApplication" Language="C#" %>
---
+++ BigSchool27/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool27.Startup))]
+namespace BigSchool27
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
